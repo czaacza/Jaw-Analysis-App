9b83272c42b3f82520dbae1854a8271485dcf708
Mati <maticzacza@gmail.com>
2022-12-02 22:17:42 +0200

    showing coordinates, calculating distance

--- src/app.py
+++ src/app.py
@@ -1,48 +1,40 @@
 import cv2
 # [print(i) for i in dir(cv2) if 'EVENT' in i]
 
-
 # function to display the coordinates of
 # of the points clicked on the image
+
+last_coordinates = None
+
 def click_event(event, x, y, flags, params):
+    global last_coordinates
+    currentCoordinates = (x,y)
+
     # checking for left mouse clicks
     if event == cv2.EVENT_LBUTTONDOWN:
-        # displaying the coordinates
-        # on the Shell
-        print(x, ' ', y)
+        if(last_coordinates is not None):
+            print(calculateDistance(last_coordinates, currentCoordinates))
+            cv2.line(img, currentCoordinates, last_coordinates, (0, 255, 0), thickness=1)
 
-        # displaying the coordinates
-        # on the image window
+        # displaying the coordinates on the image window
         font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img, str(x) + ',' +
-                    str(y), (x, y), font,
-                    1, (255, 0, 0), 2)
+        cv2.putText(img, '. ' + str(x) + ',' +
+                    str(y), currentCoordinates, font,
+                    0.5, (255, 0, 0), 2)
         cv2.imshow('image', img)
+        last_coordinates = (x,y)
 
-    # checking for right mouse clicks
-    if event == cv2.EVENT_RBUTTONDOWN:
-        # displaying the coordinates
-        # on the Shell
-        print(x, ' ', y)
 
-        # displaying the coordinates
-        # on the image window
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        b = img[y, x, 0]
-        g = img[y, x, 1]
-        r = img[y, x, 2]
-        cv2.putText(img, str(b) + ',' +
-                    str(g) + ',' + str(r),
-                    (x, y), font, 1,
-                    (255, 255, 0), 2)
-        cv2.imshow('image', img)
+def calculateDistance(p1, p2):
+    """p1 and p2 in format (x1,y1) and (x2,y2) tuples"""
+    dis = ((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
+    return dis
 
 
 # driver function
 if __name__ == "__main__":
     # reading the image
     img = cv2.imread('img.png', 1)
-    img = cv2.resize(img, (0, 0), None, 0.2, 0.2)
 
     # displaying the image
     cv2.imshow('image', img)
