087c5ef393f911d23283b36aa35dcbec7bd2de97
Mati <maticzacza@gmail.com>
2022-12-02 23:24:47 +0200

    simple app created - measures the distances

--- src/app.py
+++ src/app.py
@@ -12,29 +12,35 @@ def click_event(event, x, y, flags, params):
 
     # checking for left mouse clicks
     if event == cv2.EVENT_LBUTTONDOWN:
+        font = cv2.FONT_HERSHEY_SIMPLEX
+
         if(last_coordinates is not None):
-            print(calculateDistance(last_coordinates, currentCoordinates))
             cv2.line(img, currentCoordinates, last_coordinates, (0, 255, 0), thickness=1)
+            cv2.putText(img, str(calculateDistance(last_coordinates, currentCoordinates)), calculateMiddleOfTwoPoints(last_coordinates, currentCoordinates), font,
+                    0.5, (255, 0, 0), 2)
 
         # displaying the coordinates on the image window
-        font = cv2.FONT_HERSHEY_SIMPLEX
-        cv2.putText(img, '. ' + str(x) + ',' +
-                    str(y), currentCoordinates, font,
-                    0.5, (255, 0, 0), 2)
+        
+        
         cv2.imshow('image', img)
         last_coordinates = (x,y)
 
 
 def calculateDistance(p1, p2):
     """p1 and p2 in format (x1,y1) and (x2,y2) tuples"""
-    dis = ((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5
-    return dis
+    return  round(((p2[0] - p1[0]) ** 2 + (p2[1] - p1[1]) ** 2) ** 0.5, 2)
+    
 
+def calculateMiddleOfTwoPoints(p1, p2):
+    return (round((p2[0] + p1[0]) / 2) , round((p2[1] + p1[1]) / 2))
 
 # driver function
 if __name__ == "__main__":
+    point1 = (3, 3)
+    point2 = (-4, -7)
+    print(calculateMiddleOfTwoPoints(point1, point2))
     # reading the image
-    img = cv2.imread('img.png', 1)
+    img = cv2.imread('jaw.jpeg', 1)
 
     # displaying the image
     cv2.imshow('image', img)
